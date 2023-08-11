c5c6fbcd8bb8a3316f70bff39839c1bdb90d514d
Joaquín Kuchman <141062582+JoaquinKuchman@users.noreply.github.com>
2023-08-11 13:51:27 -0300

    final

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var miDiv = document.getElementById("miDiv");
+    
+    miDiv.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
